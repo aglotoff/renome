9e243ffd5f527fddc94a7618c2ac4c44e5e033f9
Andrey Glotov <andrei.glotoff@gmail.com>
2018-10-15 12:10:38 +0300

    start the about page

--- src/blocks/common/page/page.js
+++ src/blocks/common/page/page.js
@@ -5,19 +5,25 @@
     const STICKY_OFFSET = 100;
     const VISIBLE_OFFSET = 400;
     
+    const headerIsTransparent = $('.header').hasClass('header_transparent');
+
     // Animate sticky header
     function handleScroll() {
         const offset = $(window).scrollTop();
-    
-        $('.page__header').toggleClass(
-            'page__header_scroll',
-            offset >= STICKY_OFFSET
-        );
-    
-        $('.page__header').toggleClass(
-            'page__header_hidden',
-            (offset > STICKY_OFFSET) && (offset < VISIBLE_OFFSET)
-        );
+
+        $('.page__header')
+            .toggleClass(
+                'page__header_scroll',
+                offset >= STICKY_OFFSET
+            )
+            .toggleClass(
+                'header_transparent',
+                headerIsTransparent && (offset < STICKY_OFFSET)
+            )
+            .toggleClass(
+                'page__header_hidden',
+                (offset > STICKY_OFFSET) && (offset < VISIBLE_OFFSET)
+            );
     }
     
     $(window).on('scroll', handleScroll);
