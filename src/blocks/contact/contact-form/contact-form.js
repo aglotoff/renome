a27da5ad334427e64d7b266b6955d3e1fd5af099
Andrey Glotov <andrei.glotoff@gmail.com>
2018-10-17 16:40:12 +0300

    finish the contact page

---
+++ src/blocks/contact/contact-form/contact-form.js
@@ -0,0 +1,22 @@
+/**
+ * Contact Form Block
+ */
+$('.contact-form').validate({
+    errorClass: 'error contact-form__error',
+
+    highlight: function(element) {
+        if ($(element).hasClass('input')) {
+            $(element).addClass('input_invalid');
+        } else if ($(element).hasClass('text-area')) {
+            $(element).addClass('text-area_invalid');
+        }
+    },
+
+    unhighlight: function(element) {
+        if ($(element).hasClass('input')) {
+            $(element).removeClass('input_invalid');
+        } else if ($(element).hasClass('text-area')) {
+            $(element).removeClass('text-area_invalid');
+        }
+    },
+});
