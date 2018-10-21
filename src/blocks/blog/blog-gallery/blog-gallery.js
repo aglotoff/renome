0b54341f48a1b188fb19016e9ee5e8cfa94b6f5a
Andrey Glotov <andrei.glotoff@gmail.com>
2018-10-21 14:26:29 +0300

    add blog articles

---
+++ src/blocks/blog/blog-gallery/blog-gallery.js
@@ -0,0 +1,23 @@
+/**
+ * Blog Article Gallery Block
+ */
+(function($) {
+    const TRANSITION_SPEED = 250;
+
+    $('.blog-gallery__container').slick({
+        rows: 0,
+        prevArrow: '.blog-gallery__arrow_dir_prev',
+        nextArrow: '.blog-gallery__arrow_dir_next',
+        cssEase: 'linear',
+        dots: false,
+        speed: TRANSITION_SPEED,
+        slide: '.blog-gallery__slide',
+        zIndex: 1,
+        responsive: [{
+            breakpoint: 970,
+            settings: {
+                dots: true
+            }
+        }]
+    });
+})($);
