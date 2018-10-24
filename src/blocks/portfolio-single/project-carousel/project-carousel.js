1139c9c7a52fa30c137f3096a09f1eaab6ee0bc0
Andrey Glotov <andrei.glotoff@gmail.com>
2018-10-24 15:26:44 +0300

    add portfolio single page

---
+++ src/blocks/portfolio-single/project-carousel/project-carousel.js
@@ -0,0 +1,17 @@
+/**
+ * Project Carousel Block
+ */
+(function($) {
+    const TRANSITION_SPEED = 250;
+
+    $('.project-carousel__container').slick({
+        rows: 0,
+        prevArrow: '.project-carousel__arrow_dir_prev',
+        nextArrow: '.project-carousel__arrow_dir_next',
+        cssEase: 'linear',
+        dots: true,
+        speed: TRANSITION_SPEED,
+        slide: '.project-carousel__slide',
+        zIndex: 1,
+    });
+})($);
