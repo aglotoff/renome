46803bb73212dd08360e085e67c3b239e7f90d57
Andrey Glotov <andrei.glotoff@gmail.com>
2018-10-24 16:44:26 +0300

    add shop page

---
+++ src/blocks/shop/product/product.js
@@ -0,0 +1,20 @@
+/**
+ * Product Block
+ */
+$('.product__add-btn').click(function() {
+    const $loader = $('<span></span>')
+        .addClass('loader loader_size_s product__add-loader')
+        .insertAfter($(this));
+
+    $(this).remove();
+
+    setTimeout(function() {
+        $('<a></a>')
+            .attr('href', 'shop-cart.html')
+            .addClass('product__cart-link')
+            .text('Product added')
+            .insertAfter($loader);
+
+        $loader.remove();
+    }, 1000);
+});
---
+++ src/blocks/shop/shop-cats/shop-cats.js
@@ -0,0 +1,14 @@
+/**
+ * Product Categories Block
+ */
+$('.shop-cats__toggle').click(function() {
+    const $list = $(this).next('.shop-cats__list');
+    
+    $list.addClass('shop-cats__list_visible');
+
+    setTimeout(function() {
+        $(document).one('click', function() {
+            $list.removeClass('shop-cats__list_visible');
+        });
+    });
+});
