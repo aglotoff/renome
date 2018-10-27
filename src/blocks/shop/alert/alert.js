095cfa3cf0e8997ada747b946e82cff6a615e53d
Andrey Glotov <andrei.glotoff@gmail.com>
2018-10-27 13:25:53 +0300

    add cart form

--- src/blocks/shop/alert/alert.js
+++ src/blocks/shop/alert/alert.js
@@ -13,25 +13,44 @@
         }, SCROLL_SPEED);
     };
 
+    const showMessage = function(message) {
+        $alert
+            .html('')
+            .append($('<div></div>')
+                .addClass('alert__msg')
+                .text(message));
+        
+        showAlert();
+    };
+
+    const showMessageAndLink = function(message, link) {
+        $alert
+            .html('')
+            .append($('<div></div>')
+                .addClass('alert__msg')
+                .text(message))
+            .append($('<a></a>')
+                .addClass('alert__link')
+                .attr('href', 'shop-cart.html')
+                .text(link));
+        
+        showAlert();
+    };
+
     $('.page')
         .on('addProduct', function(event, title) {
-            $alert
-                .html('')
-                .append($('<div></div>')
-                    .addClass('alert__msg')
-                    .text(`"${title}" was successfully added to your cart.`))
-                .append($('<a></a>')
-                    .addClass('alert__cart-link')
-                    .attr('href', 'shop-cart.html')
-                    .text('View cart'));
-            showAlert();
+            showMessageAndLink(
+                `"${title}" was successfully added to your cart.`,
+                'View cart'
+            );
         })
         .on('error', function(event, message) {
-            $alert
-                .html('')
-                .append($('<div></div>')
-                    .addClass('alert__msg')
-                    .text(message));
-            showAlert();
+            showMessage(message);
+        })
+        .on('cartUpdated', function() {
+            showMessage('Cart updated.');
+        })
+        .on('productRemoved', function(event, title) {
+            showMessageAndLink(`"${title}" removed.`, 'Undo?');
         });
 })($);
