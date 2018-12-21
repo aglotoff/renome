58d3e04deb72735e7b36deca96958246528406eb
polarfawx <andrei.glotoff@gmail.com>
2018-12-22 00:36:27 +0300

    finish the about page refactoring

--- src/blocks/about/about-gallery/about-gallery.js
+++ src/blocks/about/about-gallery/about-gallery.js
@@ -17,6 +17,20 @@ export const initModule = function() {
                 enabled  : true,
                 tCounter : '',
             },
+
+            closeMarkup : '<button aria-label="Close" type="button" '
+                + 'class="mfp-close">&#215;</button>',
+            arrowMarkup : '<button aria-label="%title%" type="button" '
+                + 'class="mfp-arrow mfp-arrow-%dir%"></button>',
+
+            callbacks   : {
+                beforeOpen() {
+                    this.wrap.attr({
+                        'role'       : 'dialog',
+                        'aria-label' : 'Gallery',
+                    });
+                },
+            },
         });
     });
 
